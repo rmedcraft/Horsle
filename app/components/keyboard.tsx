1a8b7ed64a3e514825e59b618c64091f9929405b
rmedcraft <rowanmedcraft04@gmail.com>
2025-09-15 20:24:45 -0400

    every keyboard letter has a unique key

--- app/components/keyboard.tsx
+++ app/components/keyboard.tsx
@@ -2,14 +2,16 @@ import { LetterKey } from "./letterKey";
 
 export function Keyboard() {
     const keyboardLetters = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"]
+    let keyIndex = -1
 
     return (
         <div className="mx-auto mt-6 flex flex-col gap-2">
             {keyboardLetters.map((row, index) =>
-                <div className="flex flex-row mx-auto gap-1.5">
-                    {[...row].map((letter, index) =>
-                        <LetterKey key={index} letter={letter} />
-                    )}
+                <div className="flex flex-row mx-auto gap-1.5" key={index}>
+                    {[...row].map((letter, rowIndex) => {
+                        keyIndex++
+                        return <LetterKey key={keyIndex} letter={letter} />
+                    })}
                 </div>
             )}
         </div>
