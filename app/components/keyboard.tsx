c3b5faa001f01ec17f8ac16f8742a4ea433d1b3f
rmedcraft <rowanmedcraft04@gmail.com>
2025-09-15 20:55:26 -0400

    IDs for the keyboard based on the letter they represent+ title, description, and favicon

--- app/components/keyboard.tsx
+++ app/components/keyboard.tsx
@@ -2,16 +2,14 @@ import { LetterKey } from "./letterKey";
 
 export function Keyboard() {
     const keyboardLetters = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"]
-    let keyIndex = -1
 
     return (
         <div className="mx-auto mt-6 flex flex-col gap-2">
             {keyboardLetters.map((row, index) =>
                 <div className="flex flex-row mx-auto gap-1.5" key={index}>
-                    {[...row].map((letter, rowIndex) => {
-                        keyIndex++
-                        return <LetterKey key={keyIndex} letter={letter} />
-                    })}
+                    {[...row].map((letter, rowIndex) =>
+                        <LetterKey key={letter} letter={letter} />
+                    )}
                 </div>
             )}
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
 import { Navbar } from "./components/navbar";
 import { LetterGrid } from "./components/letterGrid";
 import { Keyboard } from "./components/keyboard";
 
 export default function Home() {
+  const answer = "HORSE"
+
   return (
     <div className="flex flex-col absolute top-0 w-screen h-screen">
       <Navbar />
