2f0745260e81833e0cd821c6a7789b39e6f0ff6e
rmedcraft <rowanmedcraft04@gmail.com>
2025-09-23 15:41:22 -0400

    backspace on onscreen keyboard works

--- app/components/bigKey.tsx
+++ app/components/bigKey.tsx
@@ -1,8 +1,10 @@
+import { checkInput } from "../page"
+
 export function BigKey(props: any) {
-    const { inner } = props
+    const { inner, keyCode, stateGrid, gridData } = props
 
     return (
-        <div className="cursor-pointer bg-gray-300 border-0 rounded-md w-16 h-14 flex flex-col justify-center items-center">
+        <div className="cursor-pointer bg-gray-300 border-0 rounded-md w-16 h-14 flex flex-col justify-center items-center" onClick={() => { checkInput(keyCode, stateGrid, gridData) }}>
             {inner}
         </div>
     )
--- app/components/keyboard.tsx
+++ app/components/keyboard.tsx
@@ -3,7 +3,7 @@ import { LetterKey } from "./letterKey";
 import { BsBackspace } from "react-icons/bs";
 
 export function Keyboard(props: any) {
-    const { setLetter, stateGrid } = props
+    const { stateGrid, gridData } = props
 
     const keyboardLetters = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"]
 
@@ -11,13 +11,13 @@ export function Keyboard(props: any) {
         <div className="mx-auto mt-6 flex flex-col gap-2">
             {keyboardLetters.map((row, index) =>
                 <div className="flex flex-row mx-auto gap-1.5" key={index}>
-                    {index === 2 && <BigKey inner={<p className="text-xs font-bold">ENTER</p>} />}
+                    {index === 2 && <BigKey inner={<p className="text-xs font-bold">ENTER</p>} keyCode="Enter" stateGrid={stateGrid} gridData={gridData} />}
 
                     {[...row].map((letter, rowIndex) =>
-                        <LetterKey key={letter} letter={letter} stateGrid={stateGrid} gridData={props.gridData} />
+                        <LetterKey key={letter} letter={letter} stateGrid={stateGrid} gridData={gridData} />
                     )}
 
-                    {index === 2 && <BigKey inner={<BsBackspace className="text-lg" fontWeight='bold' />} />}
+                    {index === 2 && <BigKey inner={<BsBackspace className="text-lg" fontWeight='bold' />} keyCode="Backspace" stateGrid={stateGrid} gridData={gridData} />}
                 </div>
             )}
         </div>
